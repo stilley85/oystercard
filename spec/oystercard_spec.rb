c66b67e2b10b2420f1ad3faed83a98782de0c7f2
Stephen Tilley <stephentilley@gmail.com>
2018-01-29 15:17:02 +0000

    Finished step 4: Added an initial balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,12 @@ require "oystercard"
 
 describe Oystercard do
 
+  context "#initialize" do
+    
+    it "initializes with a balance of 0" do
+      expect(subject.balance).to eq(0)
+    end
+
+  end
+
 end
