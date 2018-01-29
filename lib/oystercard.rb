1a331c3264c58ddec5f056cdac216030483d98ab
Stephen Tilley <stephentilley@gmail.com>
2018-01-29 17:27:08 +0000

    Exercise 9 completed: Check minimum balance on touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MIN_FARE = 1
 
   attr_reader :balance, :in_journey
 
@@ -19,6 +20,7 @@ class Oystercard
   end
 
   def touch_in
+    raise "insufficient funds" if insufficient_funds?
     @in_journey = true
   end
 
@@ -34,6 +36,10 @@ class Oystercard
 
   def in_journey?
     @in_journey
+  end
+
+  def insufficient_funds?
+    @balance < MIN_FARE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,63 +2,83 @@ require "oystercard"
 
 describe Oystercard do
 
-  context "#initialize" do
+  context "funds not required" do
 
-    it "initializes with a balance of 0" do
-      expect(subject.balance).to eq(0)
+    context "#initialize" do
+
+      it "initializes with a balance of 0" do
+        expect(subject.balance).to eq(0)
+      end
+
+      it "initializes as not in journey" do
+        expect(subject.in_journey).to eq(false)
+      end
+
     end
 
-    it "initializes as not in journey" do
-      expect(subject.in_journey).to eq(false)
+    context "#top_up" do
+
+        it "adds the amount to balance" do
+          subject.top_up(10)
+          expect(subject.balance).to eq(10)
+        end
+
+        it "raises an error if top up puts us over the max balance" do
+          maximum_balance = Oystercard::MAX_BALANCE
+          subject.top_up(maximum_balance)
+          expect { subject.top_up(1) }.to raise_error "Too much! The limit is #{maximum_balance}"
+        end
+
     end
 
-  end
+    context "#deduct" do
 
-  context "#top_up" do
+      it "deducts the amount from the balance" do
+        subject.deduct(10)
+        expect(subject.balance).to eq(-10)
+      end
 
-      it "adds the amount to balance" do
-        subject.top_up(10)
+      it "deducts the amount from the balance" do
+        subject.top_up(20)
+        subject.deduct(10)
         expect(subject.balance).to eq(10)
       end
 
-      it "raises an error if top up puts us over the max balance" do
-        maximum_balance = Oystercard::MAX_BALANCE
-        subject.top_up(maximum_balance)
-        expect { subject.top_up(1) }.to raise_error "Too much! The limit is #{maximum_balance}"
+    end
+
+    context "#touch_in" do
+
+      it "Doesn't allow touch in if insufficient funds" do
+        expect{ subject.touch_in }.to raise_error "insufficient funds"
       end
 
-  end
-
-  context "#deduct" do
-
-    it "deducts the amount from the balance" do
-      subject.deduct(10)
-      expect(subject.balance).to eq(-10)
     end
 
-    it "deducts the amount from the balance" do
+  end
+
+  context "funds required" do
+
+    before(:each) do
       subject.top_up(20)
-      subject.deduct(10)
-      expect(subject.balance).to eq(10)
     end
 
-  end
+    context "#touch_in" do
 
-  context "#touch_in" do
+      it "sets in journey status to true" do
+        subject.touch_in
+        expect(subject.in_journey).to eq true
+      end
 
-    it "sets in journey status to true" do
-      subject.touch_in
-      expect(subject.in_journey).to eq true
     end
 
-  end
+    context "#touch_out" do
 
-  context "#touch_out" do
+      it "sets in journey status to false" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject.in_journey).to eq false
+      end
 
-    it "sets in journey status to false" do
-      subject.touch_in
-      subject.touch_out
-      expect(subject.in_journey).to eq false
     end
 
   end
