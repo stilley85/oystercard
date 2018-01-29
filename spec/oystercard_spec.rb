411589361d68f6ec2ac98ecbd075d9aa397417fd
Stephen Tilley <stephentilley@gmail.com>
2018-01-29 15:25:43 +0000

    Step 5 in progress: Adding a top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,10 +3,16 @@ require "oystercard"
 describe Oystercard do
 
   context "#initialize" do
-    
+
     it "initializes with a balance of 0" do
       expect(subject.balance).to eq(0)
     end
+
+  end
+
+  context "#top_up" do
+
+      it { is_expected.to respond_to(:top_up) }
 
   end
 
